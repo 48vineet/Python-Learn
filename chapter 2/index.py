4fdb4442fb32e1e4e02c56caee1b45de1b8461ff
Vineet Mali <7038vineet@gmail.com>
2026-01-02 15:06:55 +0530

    Add initial content for Chapter 2, covering data types and operators in Python

---
+++ chapter 2/index.py
@@ -0,0 +1,34 @@
+
+# * Primarily these are the following data types in Python:
+# ? 1. Integers
+# ? 2. Floating point numbers
+# ? 3. Strings
+# ? 4. Booleans
+# ? 5. None
+
+# *a = 71  # identifies a as class <int>
+# *b = 88.44  # identifies b as class <float>
+# *name = "harry"  # identifies name as class <str>
+
+# * Following are some common operators in python:
+# ? 1. Arithmetic operators: +, -, *, / etc.
+# ? 2. Assignment operators:  =, +=, -= etc.
+# ? 3. Assignment operators:  =, +=, -= etc.
+# ? 4. Logical operators: and, or, not.
+
+# * a = 31
+# * type(a)  # class <int>
+
+# ? str(31) = > "31"   # integer to string conversion
+# ? int("32") = > 32  # string to integer conversion
+# ? float(32) = > 32.0  # integer to float conversion
+
+#! Practice Questions
+
+# ? 1. Write a python program to add two numbers.
+# ? 2. Write a python program to find remainder when a number is divided by z.
+# ? 3. Check the type of variable assigned using input() function.
+# ? 4. Use comparison operator to find out whether ‘a’ given variable a is greater than
+# ?     ‘b’ or not . Take a = 34 and b = 80
+# ? 5. Write a python program to find an average of two numbers entered by the user.
+# ? 6. Write a python program to calculate the square of a number entered by the user.
