62cc4f1c91a7ca05012ba48d76b86806e2ecf924
Vineet Mali <7038vineet@gmail.com>
2026-01-10 17:27:56 +0530

    Add content for Chapter 6: Conditional Expression, including syntax, examples, and practice set

---
+++ chapter 6/index.py
@@ -0,0 +1,48 @@
+# CHAPTER 6 – CONDITIONAL EXPRESSION
+
+# IF ELSE AND ELIF IN PYTHON
+# In python programming too, we must be able to execute instructions on a condition(s)
+# being met.
+# This is what conditionals are for !
+# If else and elif statements are a multiway decision taken by our program due to certain
+# conditions in our code.
+
+# * Syntax:
+# ? if (condition1):  # if condition1 is True
+# ?     print("yes")
+# ? elif (condition2):  # if condition2 is True
+# ?     print("no")
+# ? else:
+# ?     # otherwise
+# ? print("maybe")
+
+
+# * CODE EXAMPLE.
+# ? a = 22
+# ? if (a > 9):
+# ?     print("greater")
+# ? else:
+# ?     print("lesser")
+
+
+# CHAPTER 6 – PRACTICE SET
+# 1. Write a program to find the greatest of four numbers entered by the user.
+# 2. Write a program to find out whether a student has passed or failed if it requires a
+# total of 40 % and at least 33 % in each subject to pass . Assume 3 subjects and
+# take marks as an input from the user.
+# 3. A spam comment is defined as a text containing following keywords:
+# “Make a lot of money”, “buy now”, “subscribe this”, “click this”. Write a program
+# to detect these spams.
+# 4. Write a program to find whether a given username contains less than 10
+# characters or not .
+# 5. Write a program which finds out whether a given name is present in a list or not .
+# 6. Write a program to calculate the grade of a student from his marks from the
+# following scheme:
+# 90 – 100 = > Ex
+# 80 – 90 = > A
+# 70 – 80 = > B
+# 60 – 70 = >C
+# 50 – 60 = > D
+# <50
+# = > F
+# 7. Write a program to find out whether a given post is talking about “Harry” or not .
