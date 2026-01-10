85b58d10cab8ccff1201ef375d611991f990a900
Vineet Mali <7038vineet@gmail.com>
2026-01-10 11:50:03 +0530

    Add practice set for Chapter 5: Dictionaries and Sets, including exercises and examples

--- chapter 5/index.py
+++ chapter 5/index.py
@@ -83,3 +83,75 @@
 # union()                    |           Returns a set containing the union of sets
 
 # update()                   |=          Updates the set with the union of this set and others
+
+
+#! CHAPTER 5 – PRACTICE SET
+
+# 1. Write a program to create a dictionary of Hindi words with values as their English
+# translation. Provide user with an option to look it up!
+# 2. Write a program to input eight numbers from the user and display all the unique
+# numbers(once).
+# 3. Can we have a set with 18 (int) and '18' (str) as a value in it?
+# 4. What will be the length of following set s:
+# s = set()
+# s.add(20)
+# s.add(20.0)
+# s.add('20')  # length of s after these operations?
+# 5. s = {}
+# What is the type of 's'?
+# 6. Create an empty dictionary. Allow 4 friends to enter their favorite language as
+# value and use key as their names. Assume that the names are unique.
+# 7. If the names of 2 friends are same
+# what will happen to the program in problem
+# 6?
+# 8. If languages of two friends are same
+# what will happen to the program in problem
+# 6?
+# 9. Can you change the values inside a list which is contained in set S?
+# s = {8, 7, 12, "Harry", [1, 2]}
+
+
+# name = {
+#     "p1": {
+#         "we": "ham",
+#         "you": "app",
+#         "they": "wo"
+#     },
+#     "p2": {
+#         "I": "main",
+#         "boy": "ladka",
+#         "girl": "ladki"
+#     }
+# }
+
+# s = set()
+
+# for i in range(0, 9):
+#     num = int(input("Enter the no upto 8 : "))
+#     s.add(num)
+
+# print(s)
+
+
+# s = {
+#     18: "18"
+# }
+
+# print(s)
+
+# s = set()
+# s.add(20)
+# s.add(20.0)
+# s.add('20')
+
+# ss = len(s)
+# print(ss)
+
+lang = {}
+
+for i in range(0, 5):
+    key = input("Enter the your name : ")
+    val = input("Enther your fac language : ")
+    lang.update({key: val})
+
+print(lang)
